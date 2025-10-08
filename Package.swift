247b042574b4838c7eefdb95d6a250a843dcf1ad
Micah Moore <mmoore@zetetic.net>
2025-10-08 10:52:31 -0400

    * Reduces platforms version requirements to Swift Package Manager minimums: .iOS(.v12), .macOS(.v10_13), .macCatalyst(.v13), .watchOS(.v4), .tvOS(.v12), .visionOS(.v1) * Removes SQLCipher.xcframework binaryTarget from source control and references tagged GitHub release for SQLCipher.xcframework.zip binaryTarget in Package.swift instead

--- Package.swift
+++ Package.swift
@@ -6,7 +6,12 @@ import PackageDescription
 let package = Package(
     name: "SQLCipher",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13), .macCatalyst(.v13), .watchOS(.v8), .tvOS(.v15), .visionOS(.v1)
+        .iOS(.v12),
+        .macOS(.v10_13),
+        .macCatalyst(.v13),
+        .watchOS(.v4),
+        .tvOS(.v12),
+        .visionOS(.v1)
     ],
     products: [
         .library(
@@ -14,7 +19,10 @@ let package = Package(
             targets: ["SQLCipher"]),
     ],
     targets: [
-        .binaryTarget(name: "SQLCipher", path: "./Sources/SQLCipher/SQLCipher.xcframework"),
+        .binaryTarget(name: "SQLCipher",
+                      url: "https://github.com/sqlcipher/SQLCipher.swift/releases/download/4.11.0/SQLCipher.xcframework.zip",
+                      checksum: "4a2ec7b682cd9a8724c2da7d1a2c94e0ecfc8d610f8604cac3bb1dd7c23b7db6"
+                     ),
         .testTarget(
             name: "SQLCipherTests",
             dependencies: ["SQLCipher"],
